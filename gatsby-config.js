890a00a3d68596186a1988a008b5edcc51dd87fb
pvaladez <valadezpm@gmail.com>
2020-11-12 17:52:58 -0500

    Added gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,5 +149,32 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {
+          '/**/*.html': [
+            'cache-control: public',
+            'cache-control: max-age=0',
+            'cache-control: must-revalidate',
+          ],
+          '/page-data/*.json': [
+            'cache-control: public',
+            'cache-control: max-age=0',
+            'cache-control: must-revalidate',
+          ],
+          '/app-data.json': [
+            'cache-control: public',
+            'cache-control: max-age=0',
+            'cache-control: must-revalidate',
+          ],
+          '/static/*': [
+            'cache-control: public',
+            'cache-control: max-age=31536000',
+            'cache-control: immutable',
+          ],
+        },
+      },
+    },
   ],
 };
